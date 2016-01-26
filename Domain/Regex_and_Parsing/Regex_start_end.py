83d0ec54b8dfb72fc3f46b73a5d87b26d9799c5d
shbaek <asmita.bella@gmail.com>
2016-01-26 23:41:39 +0900

    2016-01-26 commit - fix Regex_start_end

--- Domain/Regex_and_Parsing/Regex_start_end.py
+++ Domain/Regex_and_Parsing/Regex_start_end.py
@@ -1,8 +1,12 @@
 import re
 
+tmp = '(-1, -1)'
 x = input()
 match = input()
 m = list(map(lambda a: (a.start(1), a.end(1) - 1), re.finditer(r'(?=(%s))' % match, x)))
 
-for x in m:
-    print(x)
+if m:
+    for x in m:
+        print(x)
+else:
+    print(tmp)
